37a7ccd4f227ca0060e9069131c9934d1a5c09b0
Ruoshi Li <iceboundrock@gmail.com>
2026-04-17 21:11:13 -0700

    feat: backport schema bucket rollout contract Signed-off-by: Ruoshi Li <iceboundrock@gmail.com>

--- test/rollout-workflows-test.sh
+++ test/rollout-workflows-test.sh
@@ -41,6 +41,10 @@ assert_file_contains "${preview_workflow}" "target_stack:"
 assert_file_contains "${preview_workflow}" "manual preview only supports the first promotion stack"
 assert_file_contains "${preview_workflow}" "runs-on: ubuntu-24.04-arm"
 assert_file_contains "${preview_workflow}" "Lychee-Technology/ltbase-deploy-workflows/.github/workflows/preview-stack.yml@main"
+assert_file_contains "${preview_workflow}" "name: Validate Pulumi stack config"
+assert_file_contains "${preview_workflow}" "./scripts/check-pulumi-stack-config.sh --stack \${{ needs.prepare.outputs.target_stack }}"
+assert_file_contains "${preview_workflow}" "name: Validate customer schemas"
+assert_file_contains "${preview_workflow}" "./scripts/publish-schemas.sh --dry-run --schema-bucket"
 assert_file_contains "${preview_workflow}" "name: Audit Cloudflare mTLS"
 assert_file_contains "${preview_workflow}" "./scripts/check-cloudflare-mtls.sh --env-file .github/mTLS-audit.env --stack"
 assert_file_contains "${preview_workflow}" 'CLOUDFLARE_ZONE_ID: ${{ vars.CLOUDFLARE_ZONE_ID }}'
@@ -69,9 +73,23 @@ assert_file_contains "${rollout_hop_workflow}" 'environment: ${{ needs.prepare.o
 assert_file_contains "${rollout_hop_workflow}" 'gh api repos/${{ github.repository }}/actions/workflows/rollout-hop.yml/dispatches'
 assert_file_contains "${rollout_hop_workflow}" "name: Audit Cloudflare mTLS"
 assert_file_contains "${rollout_hop_workflow}" "./scripts/check-cloudflare-mtls.sh --env-file .github/mTLS-audit.env --stack"
-assert_file_contains "${rollout_hop_workflow}" 'CLOUDFLARE_ZONE_ID: ${{ vars.CLOUDFLARE_ZONE_ID }}'
-assert_file_contains "${rollout_hop_workflow}" "MTLS_TRUSTSTORE_KEY: mtls/cloudflare-origin-pull-ca.pem"
-assert_file_contains "${rollout_hop_workflow}" "reconcile_managed_dsql_endpoint: true"
+assert_file_contains "${rollout_hop_workflow}" "name: Validate Pulumi stack config"
+assert_file_contains "${rollout_hop_workflow}" "./scripts/check-pulumi-stack-config.sh --stack \${{ needs.prepare.outputs.target_stack }}"
+assert_file_contains "${rollout_hop_workflow}" "name: Publish customer schemas"
+assert_file_contains "${rollout_hop_workflow}" "./scripts/publish-schemas.sh --schema-bucket"
+assert_file_contains "${rollout_hop_workflow}" "name: Validate schema bucket contract"
+assert_file_contains "${rollout_hop_workflow}" "deployment outputs schemaBucket does not match"
+assert_file_contains "${rollout_hop_workflow}" 'SCHEMA_BUCKET: ${{ vars[format('\''SCHEMA_BUCKET_{0}'\'', needs.prepare.outputs.target_stack_upper)] }}'
+assert_file_contains "${rollout_hop_workflow}" "deployment outputs missing schemaBucket"
+assert_file_contains "${rollout_hop_workflow}" "name: Ensure project"
+assert_file_contains "${rollout_hop_workflow}" "aws lambda invoke"
+assert_file_contains "${rollout_hop_workflow}" "name: Advance applied schema pointer"
+assert_file_contains "${rollout_hop_workflow}" "s3://\${SCHEMA_BUCKET}/schemas/published/manifest.json"
+assert_file_contains "${rollout_hop_workflow}" "s3://\${SCHEMA_BUCKET}/schemas/applied/manifest.json"
+assert_file_contains "${rollout_hop_workflow}" "needs.publish_schemas.result == 'success'"
+  assert_file_contains "${rollout_hop_workflow}" 'CLOUDFLARE_ZONE_ID: ${{ vars.CLOUDFLARE_ZONE_ID }}'
+  assert_file_contains "${rollout_hop_workflow}" "MTLS_TRUSTSTORE_KEY: mtls/cloudflare-origin-pull-ca.pem"
+  assert_file_contains "${rollout_hop_workflow}" "reconcile_managed_dsql_endpoint: true"
 assert_file_not_contains "${rollout_hop_workflow}" "pulumi_stack: devo"
 assert_file_not_contains "${rollout_hop_workflow}" "pulumi_stack: prod"
 
